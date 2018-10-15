e616418a902e017426606e4261c172a9bc83a4ea
Tyler Graf <tylergraf@gmail.com>
2018-10-14 21:07:33 -0600

    add es5 version for dumb webpack reasons.

---
+++ service-es5.js
@@ -0,0 +1,41 @@
+var lefetch = function(url, config){
+  var defaultConfig = {
+    headers: {
+      'content-type': 'application/json'
+    }
+  };
+
+  Object.assign(defaultConfig,config)
+
+  return window.fetch(url, defaultConfig).then(function(res){if (res.status !== 204 && res.status < 400) return res.json()});
+}
+var baseUrl = 'http://localhost:3000';
+
+window.CookbookService = {
+  categories: function(){
+    return lefetch(baseUrl+'/api/categories');
+  },
+  subcategories: function(id){
+    return lefetch(baseUrl+'/api/subcategories/'+id);
+  },
+  recipes: function(id){
+    return lefetch(baseUrl+'/api/recipes/'+id);
+  },
+  recipe: function(id){
+    return lefetch(baseUrl+'/api/recipe/'+id);
+  },
+  updateRecipe: function(recipe){
+    var config = {
+      method: 'put',
+      body: JSON.stringify(recipe)
+    }
+    return lefetch(baseUrl+'/api/recipe/'+recipe._id, config);
+  },
+  newRecipe: function(recipe){
+    var config = {
+      method: 'post',
+      body: JSON.stringify(recipe)
+    }
+    return lefetch(baseUrl+'/api/recipe/new', config);
+  }
+}
